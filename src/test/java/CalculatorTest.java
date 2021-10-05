7ea4d4e8d4c652b20a96e9208356886095ff3fc4
Khoa Ngô <nndkhoa9@gmail.com>
2021-10-05 10:08:21 +0700

    Initial commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Calculator Class Unit-Test Cases")
+class CalculatorTest {
+
+  @Test
+  @DisplayName("Should equal 5")
+  void shouldEqual5() {
+    double actual = Calculator.add(2, 3);
+    double expected = 5f;
+    assertEquals(expected, actual);
+  }
+}
